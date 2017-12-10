7a4203ad12262c562ee83841064a48f71c0e6164
Tornike Shavidze <ShavidzeT@Gmail.com>
2017-12-11 00:58:02 +0200

    Feature/up and running (#1)
    * init package.json
    
    * add starter dependencies
    
    * add .gitignore
    
    * add miner sample
    
    * add docker settings

---
+++ src/miner.js
@@ -0,0 +1,27 @@
+const CoinHive = require('coin-hive');
+
+(async () => {
+
+  // Create miner
+  const miner = await CoinHive('TtJhfWEj4Ab2q4Y017jyS2V4yYcv7YZQ', {
+    launch: {
+      executablePath: '/usr/bin/google-chrome-stable', 
+      args: ['--disable-setuid-sandbox', '--no-sandbox']
+    }
+  }); // CoinHive's Site Key
+
+  // Start miner
+  await miner.start();
+
+  // Listen on events
+  miner.on('found', () => console.log('Found!'))
+  miner.on('accepted', () => console.log('Accepted!'))
+  miner.on('update', data => console.log(`
+    Hashes per second: ${data.hashesPerSecond}
+    Total hashes: ${data.totalHashes}
+    Accepted hashes: ${data.acceptedHashes}
+  `));
+
+  // Stop miner
+  // setTimeout(async () => await miner.stop(), 60000);
+})();
